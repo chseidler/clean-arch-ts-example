76ede5b1f3477b44fc48b5176479574cb8ec2f21
Christian Seidler <ch.cseidler@gmail.com>
2023-05-28 19:03:40 -0300

    method ListCustomerUseCase implemented

--- src/usecase/customer/list/list.customer.unit.spec.ts
+++ src/usecase/customer/list/list.customer.unit.spec.ts
@@ -1,5 +1,6 @@
 import CustomerFactory from "../../../domain/customer/factory/customer.factory";
 import Address from "../../../domain/customer/value-object/address";
+import ListCustomerUseCase from "./list.customer.usecase";
 
 const customer1 = CustomerFactory.createWithAddress(
     "John Doe",
@@ -25,15 +26,15 @@ describe("Unit test for listing customer use case", () => {
         const repository = MockRepository();
         const useCase = new ListCustomerUseCase(repository);
 
-        const output = await useCase.execute();
+        const output = await useCase.execute({});
 
         expect(output.customers.length).toBe(2);
         expect(output.customers[0].id).toBe(customer1.id);
         expect(output.customers[0].name).toBe(customer1.name);
-        expect(output.customers[0].address).toBe(customer1.Address.street);
+        expect(output.customers[0].address.street).toBe(customer1.Address.street);
         expect(output.customers[1].id).toBe(customer2.id);
         expect(output.customers[1].name).toBe(customer2.name);
-        expect(output.customers[1].address).toBe(customer2.Address.street);
+        expect(output.customers[1].address.street).toBe(customer2.Address.street);
 
     })
 })
