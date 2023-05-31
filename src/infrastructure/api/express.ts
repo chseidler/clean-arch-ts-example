7c1605611adf9246654ffbf98fca48397f7a170c
Christian Seidler <ch.cseidler@gmail.com>
2023-05-30 21:46:29 -0300

    customer route implemented

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
 import { Sequelize } from 'sequelize-typescript';
 import CustomerModel from '../customer/repository/sequelize/customer.model';
+import { CustomerRoute } from './routes/customer.route';
 
 export const app: Express = express();
 app.use(express.json());
+app.use("/customer", CustomerRoute);
 
 export let sequelize: Sequelize;
 
---
+++ src/infrastructure/api/routes/customer.route.ts
@@ -0,0 +1,26 @@
+import express, {Request, Response} from 'express';
+import CustomerRepository from '../../customer/repository/sequelize/customer.repository';
+import CustomerCreateUseCase from '../../../usecase/customer/create/create.customer.usecase';
+
+export const CustomerRoute = express.Router();
+
+CustomerRoute.post('/', async (req: Request, res: Response) => {
+    const useCase = new CustomerCreateUseCase(new CustomerRepository());
+    try {
+        const customerDto = {
+            name: req.body.name,
+            address: {
+                street: req.body.address.street,
+                city: req.body.address.city,
+                number: req.body.address.number,
+                zip: req.body.address.zip,
+            },
+        }
+
+        const output = await useCase.execute(customerDto);
+        res.send(output);
+    } catch(err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+})
