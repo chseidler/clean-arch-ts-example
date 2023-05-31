5b500ca3f863561d2abaab927efbbf82f31868df
Christian Seidler <ch.cseidler@gmail.com>
2023-05-30 21:52:54 -0300

    (TDD) test to list customers method

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -41,4 +41,44 @@ describe("E2E test for customer", () => {
 
         expect(response.status).toBe(500);
     });
+
+    it("should list all customers", async () => {
+        const response = await request(app)
+            .post("/customer")
+            .send({
+                name: "John",
+                address: {
+                    street: "Street",
+                    city: "City",
+                    number: 123,
+                    zip: "1234",
+                },
+            });
+
+        expect(response.status).toBe(200);
+
+        const response2 = await request(app)
+            .post("/customer")
+            .send({
+                name: "Jane",
+                address: {
+                    street: "Street2",
+                    city: "City2",
+                    number: 123,
+                    zip: "12342",
+                },
+            });
+
+        expect(response2.status).toBe(200);
+
+        const listResponse = await request(app).get("/customer").send();
+        expect(listResponse.status).toBe(200);
+        expect(listResponse.body.customers.length).toBe(2);
+        const customer = listResponse.body.customers[0];
+        expect(customer.name).toBe("John");
+        expect(customer.address.street).toBe("Street");
+        const customer2 = listResponse.body.customers[1];
+        expect(customer.name).toBe("Jane");
+        expect(customer.address.street).toBe("Street2");
+    });
 });
